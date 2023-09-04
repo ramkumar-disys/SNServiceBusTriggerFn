7068e81472204af84f71f59b6088f1e85793e5de
ramkumar-disys <70595603+ramkumar-disys@users.noreply.github.com>
2023-09-04 14:48:43 +0000

    Update Function1.cs

--- Function1.cs
+++ Function1.cs
@@ -7,6 +7,10 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 
 namespace SNRecieverWebHook
 {
